0f72dc65b80644253499bc7d9c659d29fd8f1e24
smanish01 <hjaan.jani.3592@usachan.cf>
2018-05-11 19:05:43 +0530

    ui alomost done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Login from './Login';
 import Signup from './Signup';
-import Notes from './Notes';
 import Twitter from './Twitter';
+import Viewnotes from './Viewnotes';
+import Notes1 from './Notes1';
 import Addnotes from './Addnotes';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 const { Header, Content, Footer } = Layout;
@@ -25,7 +26,7 @@ class App extends Component {
                 <Menu.Item key="1"><Link to={'/Login'}>Login</Link></Menu.Item>
                 <Menu.Item key="2"><Link to={'/Signup'}>Signup</Link></Menu.Item>
                 <Menu.Item key="3"><Link to={'/Twitter'}>Signin with <Icon type="twitter" /></Link></Menu.Item>
-                <Menu.Item key="4"><Link to={'/Notes'}>Notes</Link></Menu.Item>
+                <Menu.Item key="4"><Link to={'/Viewnotes'}>Viewnotes</Link></Menu.Item>
                 <Menu.Item key="5"><Link to={'/Addnotes'}><Icon type="plus-circle" /></Link></Menu.Item>
               </Menu>
             </Header>
@@ -34,8 +35,9 @@ class App extends Component {
             <Route exact path='/Login' component={Login} />
             <Route exact path='/Signup' component={Signup} />
             <Route exact path='/Twitter' component={Twitter} />
-            <Route exact path='/Notes' component={Notes} />
+            <Route exact path='/Viewnotes' component={Viewnotes} />
             <Route exact path='/Addnotes' component={Addnotes} />
+            <Route exact path='/Notes1' component={Notes1} />
           </Switch>
         </div>
       </Router >
