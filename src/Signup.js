47d483d4e8566777c7992b798482c44997b9b39f
smanish01 <hjaan.jani.3592@usachan.cf>
2018-05-15 19:23:50 +0530

    understand cookie and flow of app

--- src/Signup.js
+++ src/Signup.js
@@ -5,131 +5,53 @@ import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Butto
 const FormItem = Form.Item;
 
 class Signup extends React.Component {
-    state = {
-        confirmDirty: false,
-        autoCompleteResult: [],
-    };
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.form.validateFieldsAndScroll((err, values) => {
+        this.props.form.validateFields((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
             }
         });
     }
-    handleConfirmBlur = (e) => {
-        const value = e.target.value;
-        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-    }
-    compareToFirstPassword = (rule, value, callback) => {
-        const form = this.props.form;
-        if (value && value !== form.getFieldValue('password')) {
-            callback('Two passwords that you enter is inconsistent!');
-        } else {
-            callback();
-        }
-    }
-    validateToNextPassword = (rule, value, callback) => {
-        const form = this.props.form;
-        if (value && this.state.confirmDirty) {
-            form.validateFields(['confirm'], { force: true });
-        }
-        callback();
-    }
 
     render() {
         const { getFieldDecorator } = this.props.form;
-        const { autoCompleteResult } = this.state;
-
-        const formItemLayout = {
-            labelCol: {
-                xs: { span: 24 },
-                sm: { span: 8 },
-            },
-            wrapperCol: {
-                xs: { span: 24 },
-                sm: { span: 14 },
-            },
-        };
-        const tailFormItemLayout = {
-            wrapperCol: {
-                xs: {
-                    span: 24,
-                    offset: 0,
-                },
-                sm: {
-                    span: 16,
-                    offset: 8,
-                },
-            },
-        };
-
         return (
             <div id="middlePageDesign">
-                <Form onSubmit={this.handleSubmit}>
-
-                    <FormItem
-                        {...formItemLayout}
-                        label="E-mail"
-                    >
+                <Form onSubmit={this.handleSubmit} className="login-form">
+                    <FormItem>
+                        {getFieldDecorator('name', {
+                            rules: [{ required: true, message: 'Please input your Name!' }],
+                        })(
+                            <Input prefix={<Icon type="smile-o" style={{ color: 'rgba(0,0,0,.25)' }} />} type="text" placeholder="Name" />
+                        )}
+                    </FormItem>
+                    <FormItem>
                         {getFieldDecorator('email', {
-                            rules: [{
-                                type: 'email', message: 'The input is not valid E-mail!',
-                            }, {
-                                required: true, message: 'Please input your E-mail!',
-                            }],
+                            rules: [{ required: true, message: 'Please input your email!' }],
                         })(
-                            <Input />
+                            <Input prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />} type="text" placeholder="Email" />
                         )}
                     </FormItem>
-                    <FormItem
-                        {...formItemLayout}
-                        label={(
-                            <span>
-                                Username&nbsp;
-              <Tooltip title="What do you want others to call you?">
-                                    <Icon type="question-circle-o" />
-                                </Tooltip>
-                            </span>
-                        )}
-                    >
-                        {getFieldDecorator('nickname', {
-                            rules: [{ required: true, message: 'Please input your username!', whitespace: true }],
+                    <FormItem>
+                        {getFieldDecorator('userName', {
+                            rules: [{ required: true, message: 'Please input your username!' }],
                         })(
-                            <Input />
+                            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
                         )}
                     </FormItem>
-                    <FormItem
-                        {...formItemLayout}
-                        label="Password"
-                    >
+                    <FormItem>
                         {getFieldDecorator('password', {
-                            rules: [{
-                                required: true, message: 'Please input your password!',
-                            }, {
-                                validator: this.validateToNextPassword,
-                            }],
+                            rules: [{ required: true, message: 'Please input your Password!' }],
                         })(
-                            <Input type="password" />
-                        )}
-                    </FormItem>
-                    <FormItem
-                        {...formItemLayout}
-                        label="Confirm Password"
-                    >
-                        {getFieldDecorator('confirm', {
-                            rules: [{
-                                required: true, message: 'Please confirm your password!',
-                            }, {
-                                validator: this.compareToFirstPassword,
-                            }],
-                        })(
-                            <Input type="password" onBlur={this.handleConfirmBlur} />
+                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
                         )}
                     </FormItem>
 
-                    <FormItem {...tailFormItemLayout}>
-                        <Button type="primary" htmlType="submit">Register</Button>
+                    <FormItem>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Sign Up
+                        </Button>
                     </FormItem>
                 </Form>
             </div>
